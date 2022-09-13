c8bc0b89fe47b05668bdb6f726fae83096e7ceb9
Connor Hellman <108197430+Connoreh@users.noreply.github.com>
2022-09-12 20:24:22 -0500

    Studio-Unit-Testing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,58 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void oneOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void oneClosedBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void multipleBalancedBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
+    @Test
+    public void balancedBracketplusOpenReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+    }
+
+    @Test
+    public void balancedBracketplusClosedReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void balancedBracketWithStuffInBetween(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[ereasrs]"));
+    }
+    @Test
+    public void balancedBracketWithContentAndBracketsS(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[eras]er"));
+    }
+
+    @Test
+    public void nestedBracketsRetrunsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Launch]]"));
+    }
+
+    @Test
+    public void reverseOnlyReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void stringWithNoBraces(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
 
 }
